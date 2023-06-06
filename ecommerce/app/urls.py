4e9d5ab7001268e7971c2a557bee4f8935c6a5a6
siddhiagarkar <siddhiagarkar1203@gmail.com>
2023-06-06 22:35:46 +0545

    added sort by price functionality

--- ecommerce/app/urls.py
+++ ecommerce/app/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('searcheditems/', views.searchview, name='searched_item'),
     path('genderfiltereditems/', views.filterview, name='products_by_gender'),
     path('colourfiltereditems/', views.colorview, name='products_by_colours'),
+    path('sort-by/', views.sortview, name='sorted_products'),
     path('register/login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('register/', views.registration_view, name='register'),
--- ecommerce/app/views.py
+++ ecommerce/app/views.py
@@ -5,6 +5,7 @@ import json
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def store(request):
     things = Product.objects.all()
@@ -75,6 +76,7 @@ def searchview(request):
 
 def filterview(request):
     things = Product.objects.all()
+    print('f')
     mylist=request.GET.getlist('gendercheck')
     if 'Male' in mylist:
         things=Product.objects.filter(tag=False)
@@ -86,6 +88,28 @@ def filterview(request):
     colorzz = list(Product.objects.all().values('color').distinct())
     colorz = [u['color'] for u in colorzz]
     context = {'things': things, 'colorz': colorz}
+    return render(request,'store.html',context)
+
+def sortview(request):
+    things = Product.objects.all()
+    print("in")
+    if request.method == 'POST':
+        sortby=request.POST.get('sort')
+        print(sortby)
+        if sortby == 'Price: Low to High':
+            things = Product.objects.all().order_by('price')
+        elif sortby == 'Price: High to Low':
+            things = Product.objects.all().order_by('-price')
+        # elif sortby == 'disc':
+        #     things = Product.objects.all().order_by('item')
+        # elif sortby == 'rating':
+        #     things = Product.objects.all().order_by('-item')
+        else:
+            things = Product.objects.all()
+
+    # colorzz = list(Product.objects.all().values('color').distinct())
+    # colorz = [u['color'] for u in colorzz]
+    context = {'things': things}
     return render(request,'store.html',context)
 
 def colorview(request):
@@ -117,7 +141,9 @@ def login_view(request):
         # Redirect to a success page.
         return redirect('store')
     else:
-        print('USER NOT FOUND')
+        alert = 'Invalid credentials'
+        m = messages.error(request, 'Error')
+        message = str(m)
 
     context = {}
     return render(request, 'login.html', context)
@@ -137,9 +163,11 @@ def registration_view(request):
         if form.is_valid():
             fld = Customer.objects.create(userr = u, name = n, email = e)
             form.save()
-
             #log the user in
             return redirect('login')
+        else:
+            messages.error(request, 'Error')
     else:
         form = UserCreationForm()
     return render(request, 'registration.html', {'form': form})
+
